d0c89cf1f323b7c0ec5ca4112acc0af2509a209f
Akhilesh Chouhan <chouhanakhilesh999@gmail.com>
2025-12-22 16:00:45 +0530

    Leetcode for 22 th december done

--- Array/Sliding-Window/BestTimetoBuyandSellStockusingStrategy.cpp
+++ Array/Sliding-Window/BestTimetoBuyandSellStockusingStrategy.cpp
@@ -3,54 +3,69 @@ using namespace std;
 
 class Solution {
 public:
+
+ using ll = long long ; 
+
     long long maxProfit(vector<int>& prices, vector<int>& strategy, int k){
-        
-        int n = prices.size() ; 
 
-        long long result = 0 ;
+     int n = prices.size() ; 
 
-        vector<int>prefix(n , 0) ;
+     vector<ll>prefixPriceStrategy(n+1,0) ;
+     vector<ll>prefixPrice(n+1,0) ; 
 
-        prefix[0] = prices[0]*strategy[0] ; 
-
-        for(int i = 1 ; i < n ; i ++){
-            prefix[i] = prefix[i-1] + prices[i] * strategy[i] ; 
-        }
-
-        int i = 0 ; 
-        int j = 0 ; 
+     for(int i = 0 ; i < n ; i ++ ){
+          prefixPrice[i+1] = prefixPrice[i] + prices[i] ;
+          prefixPriceStrategy[i+1] = prefixPriceStrategy[i] + strategy[i]*prices[i] ; 
+     } 
 
 
-        while( j < n ){
+     ll totalSum = prefixPriceStrategy.back() ; 
+     ll result = totalSum ; 
+     int i = 0 ; 
 
-            if(j - i + 1 == k){
-                long long sum =  
-            }
+     for(int j = k - 1 ; j < n ; j ++ ){
 
+          if( j - i + 1 >= k){
 
-            j ++ ; 
-        }
+               ll windowSumOfkElement = prefixPriceStrategy[j+1] - prefixPriceStrategy[i] ; 
+               ll HalfKElementSum = prefixPrice[j+1] - prefixPrice[j - k / 2+1] ;
+               ll sol = totalSum - windowSumOfkElement  + HalfKElementSum ; 
 
+               result = max(result , sol) ;
+               i ++  ;
+          }
+     }
 
+     return result ; 
 
+       
     }
 };
 
+
 int main() {
-
-
-    int n;
-    cin >> n;
-
-    vector<int> prices(n), strategy(n);
-    for (int i = 0; i < n; i++) cin >> prices[i];
-    for (int i = 0; i < n; i++) cin >> strategy[i];
-
-    int k;
-    cin >> k;
-
     Solution sol;
-    cout << sol.maxProfit(prices, strategy, k);
+
+    // -------- Test Case 1 --------
+    vector<int> prices1   = {4, 2, 8};
+    vector<int> strategy1 = {-1, 0, 1};
+    int k1 = 2;
+
+    cout << "Test Case 1 Output: "
+         << sol.maxProfit(prices1, strategy1, k1)
+         << endl;
+
+    // -------- Test Case 2 --------
+    vector<int> prices2   = {5, 4, 3};
+    vector<int> strategy2 = {1, 1, 0};
+    int k2 = 2;
+
+    cout << "Test Case 2 Output: "
+         << sol.maxProfit(prices2, strategy2, k2)
+         << endl;
 
     return 0;
 }
+
+// Time Complexity : O(n)
+// Space Complexity : O(n)
